c8a93a6346d16d7b43d3be2119dc369f63c5ffc3
Daniel Santana <daniel.santana.1661@gmail.com>
2019-04-10 12:24:40 -0300

    Added "functionality" to "Mostrar Modal" button

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded',(event)=>{
-    document.getElementById('button1').addEventListener('click',(event)=>{
-        document.getElementById('modal1').classList.add('is-active');
-        console.log( document.getElementById('modal1').classList)
-    })
+    let buttonList = document.getElementsByClassName('button');
+    for (let button of buttonList) {
+        if (button.classList[1] == 'is-primary') {
+            button.addEventListener('click',(event)=>{
+                document.getElementById('modal1').classList.add('is-active');
+                console.log(document.getElementById('modal1').classList);
+            })
+        }
+    }
 })
 
 function closeModal(){
     document.getElementById('modal1').classList.remove('is-active')
-    console.log( document.getElementById('modal1').classList)
+    console.log(document.getElementById('modal1').classList)
 }
+
